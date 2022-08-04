56728b65b4af73a53a9ac2e38fcad031cfc6a301
Matheus <mf635097@gmail.com>
2022-08-04 10:01:53 -0300

    Criado component logo com link

---
+++ src/components/LogoLink/styles.js
@@ -0,0 +1,16 @@
+import styled, { css } from 'styled-components';
+
+export const Container = styled.a`
+  ${({ theme }) => css`
+    display: flex;
+    align-items: center;
+    text-decoration: none;
+    color: inherit;
+    height: 10rem;
+
+    img {
+      max-height: 3rem;
+
+    }
+  `}
+`;
